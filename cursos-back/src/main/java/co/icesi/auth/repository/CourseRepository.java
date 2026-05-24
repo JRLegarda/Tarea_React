cc1ae4cfdbf431f5ff718388849dd00de0b0a021
andresMC21 <andrescmm0306@gmail.com>
2026-05-24 09:40:01 -0500

    Backend added

---
+++ cursos-back/src/main/java/co/icesi/auth/repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package co.icesi.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.icesi.auth.model.Course;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    Course findByName(String name);
+
+}
